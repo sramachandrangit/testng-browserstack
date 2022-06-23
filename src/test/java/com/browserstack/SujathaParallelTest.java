77944f949e7c6f19f9f4a6b9501147739795c7f2
sramachandrangit <102570499+sramachandrangit@users.noreply.github.com>
2022-06-23 15:45:13 -0600

    added  a sample test file

---
+++ src/test/java/com/browserstack/SujathaParallelTest.java
@@ -0,0 +1,43 @@
+package com.browserstack;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WebDriver;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SujathaParallelTest extends BrowserStackTestNGTest {
+
+    @Test
+    public void test() throws Exception {
+    	  
+        // navigate to Amazon
+        driver.get("https://www.Landonhomes.com");
+                    
+        
+        // Check the contact details on Landonhomes page
+        driver.findElement(By.xpath("/html/body/div[2]/div/div[2]")).isDisplayed();
+        String NewReleaseText = driver.findElement(By.xpath("/html/body/div[2]/div/div[2]")).getText();
+        Assert.assertEquals(NewReleaseText, "Chat with Landon!  |  469-804-5534");
+
+       
+        // Check the search community
+        driver.findElement(By.xpath("/html/body/div[5]/div/div/h1")).isDisplayed();
+        String CommunityButtonName= driver.findElement(By.xpath("/html/body/div[5]/div/div/h1")).getText();
+        Assert.assertEquals(CommunityButtonName, "I’m looking for a...");
+        
+        // Check the available community link and click it
+        WebDriverWait wait = new WebDriverWait(driver,30);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//html/body/div[5]/div/div/div/ul/li[1]/a")));
+        Assert.assertTrue(driver.findElement(By.xpath("//html/body/div[5]/div/div/div/ul/li[1]/a")).isDisplayed());
+       // driver.findElement(By.xpath("//html/body/div[5]/div/div/div/ul/li[1]/a")).click();
+        
+       // check the result in the new page
+        //driver.findElement(By.xpath("//*[@id=\"comm-blocks\"]/li[1]/div/div[2]/div/div/div/div[3]/div/div/div[2]/div[3]/div[2]/a")).isDisplayed();
+       
+    }
+}
